dde90268353d14373cd8f8173ecbc1a738497f50
Soren Macbeth <soren@dopeness.org>
2011-10-06 00:22:37 -0700

    updated to clojure 1.3.0

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :java-fork "true"
   :java-debug "true"
   :description "simple cascalog wrapper for cascading.hbase"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.sorenmacbeth/cascading.hbase "0.1-SNAPSHOT"]
-                 [cascalog "1.8.1"]]
+                 [cascalog "1.8.2"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [org.apache.hbase/hbase "0.90.4"]
                      [lein-midje "1.0.3"]
                      [midje-cascalog "0.2.1"]]
-  :namespaces :all)
+  :aot :all)
 
