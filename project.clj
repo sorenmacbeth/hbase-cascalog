50802e106114bdec4365834e41b12c7ad31d87b3
Soren Macbeth <soren@dopeness.org>
2012-11-27 22:06:40 -0800

    bump to lein 2, update deps

--- project.clj
+++ project.clj
@@ -1,14 +1,19 @@
-(defproject hbase.cascalog "0.1-SNAPSHOT"
-  :source-path "src/clj"
-  :java-fork "true"
-  :java-debug "true"
+(defproject hbase.cascalog "0.2-SNAPSHOT"
+  :source-paths ["src/clj"]
   :description "simple cascalog wrapper for cascading.hbase"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.sorenmacbeth/cascading.hbase "0.1-SNAPSHOT"]
-                 [cascalog "1.8.2"]]
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [org.apache.hbase/hbase "0.90.4"]
-                     [lein-midje "1.0.3"]
-                     [midje-cascalog "0.2.2"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojars.sorenmacbeth/cascading.hbase "1.2.9-SNAPSHOT"]
+                 [cascalog "1.10.1-SNAPSHOT"]
+                 [org.apache.thrift/libthrift "0.8.0"]]
+  :profiles {:provided
+             {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                             [org.apache.hbase/hbase "0.92.0"
+                              :exclusions [org.apache.thrift/libthrift]]]}
+             :dev
+             {:dependencies [[midje "1.4.0"]
+                             [midje-cascalog "0.4.0"
+                              :exclusions [org.clojure/clojure]]]}}
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
+  :min-lein-version "2.0.0"
   :aot :all)
 
