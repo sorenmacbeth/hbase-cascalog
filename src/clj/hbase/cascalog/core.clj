9c0cd344304da3156af563dcce172e83a6e5ae1f
Soren Macbeth <soren@dopeness.org>
2012-11-30 07:55:37 -0800

    remove setting the scan options in the tap definitions

--- src/clj/hbase/cascalog/core.clj
+++ src/clj/hbase/cascalog/core.clj
@@ -3,13 +3,11 @@
   (:require [cascalog.workflow :as w])
   (:import [cascading.hbase HBaseTap HBaseScheme HBaseDynamicScheme]))
 
-(defn hbase-tap [table-name scan-options key-field column-family & value-fields]
+(defn hbase-tap [table-name key-field column-family & value-fields]
   (let [scheme (HBaseScheme. (w/fields key-field) column-family (w/fields value-fields))]
-    (with-job-conf scan-options
-      (HBaseTap. table-name scheme))))
+    (HBaseTap. table-name scheme)))
 
-(defn dynamic-hbase-tap [table-name scan-options key-field value-field & column-families]
+(defn dynamic-hbase-tap [table-name key-field value-field & column-families]
   (let [scheme (HBaseDynamicScheme. (w/fields key-field) (w/fields value-field) (into-array String column-families))]
-    (with-job-conf scan-options
-      (HBaseTap. table-name scheme))))
+    (HBaseTap. table-name scheme)))
 
