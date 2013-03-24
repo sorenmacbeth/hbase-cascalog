1d06f4adf2cd6606c219b54b340552b2658cad6b
Soren Macbeth <soren@dopeness.org>
2013-03-23 22:31:55 -0700

    bump deps

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
 (defproject hbase.cascalog "0.2-SNAPSHOT"
   :source-paths ["src/clj"]
   :description "simple cascalog wrapper for cascading.hbase"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.sorenmacbeth/cascading.hbase "1.2.9-SNAPSHOT"]
-                 [cascalog "1.10.0"]
+                 [cascalog/cascalog-core "1.10.1"]
                  [org.apache.thrift/libthrift "0.8.0"
                   :exclusions
                   [org.slf4j/slf4j-api]]]
   :profiles {:provided
-             {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+             {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2"]
                              [org.apache.hbase/hbase "0.92.0"
-                              :exclusions [org.apache.thrift/libthrift]]]}
+                              :exclusions [org.apache.thrift/libthrift
+                                           org.slf4j/slf4j-api]]]}
              :dev
-             {:dependencies [[midje "1.5-alpha9"]
-                             [midje-cascalog "0.4.0"
-                              :exclusions [org.clojure/clojure]]]}}
-  :plugins [[lein-midje "3.0-alpha4"]]
+             {:dependencies [[midje "1.5.0"]
+                             [cascalog/midje-cascalog "1.10.1"]]
+              :plugins [[lein-midje "3.0.0"]]}}
   :min-lein-version "2.0.0"
   :aot :all)
