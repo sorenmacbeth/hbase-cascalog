5439ce7d259f60eb01f8c05abb9563eeb69553eb
Soren Macbeth <soren@dopeness.org>
2013-09-17 13:33:42 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hbase.cascalog "0.2-SNAPSHOT"
+(defproject hbase.cascalog "0.2.0"
   :source-paths ["src/clj"]
   :description "simple cascalog wrapper for cascading.hbase"
   :dependencies [[org.clojure/clojure "1.5.1"]
